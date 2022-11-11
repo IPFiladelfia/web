ec89366569942ba690036da8ffcbcd52bd6a0321
ramosht <guilhermeht.ramos@gmail.com>
2022-11-11 14:36:50 -0300

    setting font using @next/font

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,5 +4,10 @@ module.exports = {
   theme: {
     extend: {},
   },
+  experimental: {
+    fontLoaders: [
+      { loader: "@next/font/google", options: { subsets: ["latin"] } },
+    ],
+  },
   plugins: [],
 };
